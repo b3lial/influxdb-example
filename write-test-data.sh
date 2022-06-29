355a9242c85002bd3c63e9b68719475fcc60a237
Dr. Christian Ammann <christian.ammann@phobosys.de>
2022-06-29 23:37:05 +0200

    print out data to shell before storing in db

--- write-test-data.sh
+++ write-test-data.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
+temp="$((20 + $RANDOM % 6))"
+hum="$((40 + $RANDOM % 11))"
+echo writing into temp==$temp and hum==$hum into db
+
 curl --request POST \
 "http://localhost:8086/api/v2/write?org=phobosys&bucket=cloudsensor&precision=ns" \
   --header "Authorization: Token e3RStgZQbd9dbSiAQsWVJRxuxF6k-G7LUHMWc4KeT7iHh3-_W36-bJRWa4hBBBe8TBtBGxDCwPkg2b6DCO9asg==" \
   --header "Content-Type: text/plain; charset=utf-8" \
   --header "Accept: application/json" \
   --data-binary "
-    buero temperature=73.97038159354763,humidity=35.23103248356096
+    buero temperature=$temp,humidity=$hum
     "
