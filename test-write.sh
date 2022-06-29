5b317895f395d9726aaecbcf33606e626a1c9c62
Dr. Christian Ammann <christian.ammann@phobosys.de>
2022-06-29 19:05:52 +0200

    timestamp == system time

--- test-write.sh
+++ test-write.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
+timestamp=`date +%s%N`
+echo $timestamp
+
 curl --request POST \
 "http://localhost:8086/api/v2/write?org=phobosys&bucket=cloudsensor&precision=ns" \
   --header "Authorization: Token e3RStgZQbd9dbSiAQsWVJRxuxF6k-G7LUHMWc4KeT7iHh3-_W36-bJRWa4hBBBe8TBtBGxDCwPkg2b6DCO9asg==" \
   --header "Content-Type: text/plain; charset=utf-8" \
   --header "Accept: application/json" \
-  --data-binary '
-    airSensors,sensor_id=TLM0201 temperature=73.97038159354763,humidity=35.23103248356096 1630424257000000000
-    airSensors,sensor_id=TLM0202 temperature=75.30007505999716,humidity=35.651929918691714 1630424257000000000
-    '
+  --data-binary "
+    buero temperature=73.97038159354763,humidity=35.23103248356096 $timestamp
+    "
